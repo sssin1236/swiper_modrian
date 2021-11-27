eaf04e073d54745637c76399e3075137822b10b3
sssin1236 <suj0358@gmail.com>
2021-11-27 19:51:09 +0900

    프로젝트 시작

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const menu = ["About", "Portfolio", "Contact Me", "Video"];
+
+const swiper = new Swiper("#swiper", {
+    loop: true,
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 50,
+    mousewheel: true,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: true,
+        renderBullet: function(index, className){
+            return `<span class="${className}">${menu[index]}</span>`
+        }
+    }
+});
+
+
+const bgs = document.querySelectorAll(".bg li");
+const prev = document.querySelector(".swiper-button-prev");
+const next = document.querySelector(".swiper-button-next");
+const navi = document.querySelectorAll(".swiper-pagination span");
+
+next.addEventListener("click", activation);
+prev.addEventListener("click", activation);
+window.addEventListener("mousewheel", activation);
+swiper.on("slideChangeTransitionEnd", activation);
+
+for(let el of navi){
+    el.addEventListener("click", e=>{
+        const isOn = e.currentTarget.classList.contains("swiper-pagination-bullet-active");
+        if(isOn) return;
+        swiper.on("slideChangeTransitionEnd", activation);
+    })
+}
+
+function activation(){
+    let item = document.querySelector(".swiper-slide-active");
+    let i = item.getAttribute("data-swiper-slide-index");
+
+    for(let el of bgs){
+        el.classList.remove("on");
+    }
+    bgs[i].classList.add("on");
+}
